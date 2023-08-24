e1cc7006f42ea1edc519f1ce3d4a12dd74be04c5
Martin <martin.delimaa@gmail.com>
2023-08-24 17:03:34 +0200

    Visual rework to make the site more attractive
    Reworked the scroll effect, added an animated background, added a swiper for the projects, tracked sections in the header, and changed hover effect on .link class.

--- scripts.js
+++ scripts.js
@@ -1,22 +1,50 @@
-enableScrollListener();
 setInterval(updateClock, 100);
+let sections;
+let menuItems;
+
+document.addEventListener('DOMContentLoaded', function () {
+  initSwiper();
+  sections = document.querySelectorAll('.tracked-section');
+  menuItems = document.querySelectorAll('.menu-item');
+  enableScrollListener();
+});
+
+function updateTrackedSections() {
+  let current = '';
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (sectionTop <= window.pageYOffset + 50 && sectionTop + sectionHeight > window.pageYOffset + 50) {
+      current = section.getAttribute('id');
+    }
+  });
+  menuItems.forEach(item => {
+    item.classList.remove('active');
+    if (item.classList.contains(current)) {
+      item.classList.add('active');
+    }
+  });
+}
 
 function resizeHeader() {
   var header = document.getElementById("header")
-  var toggle= document.getElementById("toggle-switch")
+  var toggle = document.getElementById("toggle-switch")
   var scrollTreshold = 50;
   var scrollValue = window.pageYOffset || document.documentElement.scrollTop;
-  if (scrollValue > scrollTreshold || scrollValue > scrollTreshold) { 
+  if (scrollValue > scrollTreshold) {
     header.classList.add('small-header');
     toggle.classList.add('small-toggle');
-  } else{
+  } else {
     header.classList.remove('small-header');
     toggle.classList.remove('small-toggle');
   }
 }
 
 function enableScrollListener() {
-  window.addEventListener('scroll', function() {resizeHeader()});
+  window.addEventListener('scroll', function () { 
+    resizeHeader();
+    updateTrackedSections();
+  });
 }
 
 function toggleTheme() {
@@ -39,13 +67,45 @@ function updateClock() {
   hours = hours < 10 ? "0" + hours : hours;
   minutes = minutes < 10 ? "0" + minutes : minutes;
   seconds = seconds < 10 ? "0" + seconds : seconds;
-  
+
   document.getElementById("hours").innerHTML = hours;
   document.getElementById("minutes").innerHTML = " : " + minutes;
-  document.getElementById("seconds").innerHTML = " : " +  seconds;
+  document.getElementById("seconds").innerHTML = " : " + seconds;
 }
 
 function toggleDropdown() {
   var dropdown = document.getElementById("dropdown");
   dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
+}
+
+function initSwiper() {
+  const swiper = new Swiper('.swiper', {
+    loop: true,
+    slidePerView: 1,
+    centeredSlides: true,
+    spaceBetween: 50,
+
+    autoplay: {
+      delay: 5000,
+    },
+
+    keyboard : {
+      enabled: true,
+      onlyInViewport: false,
+    },
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+  });
+}
+
+function scrollToYPosition(yPosition) {
+  window.scrollTo(0, yPosition);
 }
