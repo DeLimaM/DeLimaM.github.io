ac1e805637fd4a50314a8ce39bf1e7fec0364d25
Martin <martin.delimaa@gmail.com>
2024-04-05 09:29:36 +0200

    Added the experience section, and sight changes

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
 setInterval(updateClock, 100);
 
 //on page load
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
   // initialize swipers
-  initSwiper('edu');
-  initSwiper('perso');
+  initSwiper("edu");
+  initSwiper("perso");
 
   // set the saved theme
   setSavedTheme();
@@ -12,87 +12,105 @@ document.addEventListener('DOMContentLoaded', function () {
   // update projects links based on the device
   updateProjectsLinks();
 
+  // update dynamic texts
+  updateDynamicTexts();
+
   // Add scroll events
-  window.addEventListener('scroll', function () {
+  window.addEventListener("scroll", function () {
     resizeHeader();
     refreshProgressBar();
   });
 
   // Add click events
-  document.getElementById('hamburger').addEventListener('click', function () {
+  document.getElementById("hamburger").addEventListener("click", function () {
     toggleDropdown();
   });
-  document.querySelectorAll('.menu-item-about').forEach(item => {
-    item.addEventListener('click', function () {
-      scrollToSection('about');
+  document.querySelectorAll(".menu-item-about").forEach((item) => {
+    item.addEventListener("click", function () {
+      scrollToSection("about");
     });
   });
-  document.querySelectorAll('.menu-item-projects').forEach(item => {
-    item.addEventListener('click', function () {
-      scrollToSection('projects-perso');
+  document.querySelectorAll(".menu-item-projects").forEach((item) => {
+    item.addEventListener("click", function () {
+      scrollToSection("projects-perso");
     });
   });
-  document.querySelectorAll('.menu-item-contact').forEach(item => {
-    item.addEventListener('click', function () {
-      scrollToSection('contact');
+  document.querySelectorAll(".menu-item-contact").forEach((item) => {
+    item.addEventListener("click", function () {
+      scrollToSection("contact");
     });
   });
-  document.querySelectorAll('.menu-item-dropdown').forEach(item => {
-    item.addEventListener('click', function () {
+  document.querySelectorAll(".menu-item-dropdown").forEach((item) => {
+    item.addEventListener("click", function () {
       toggleDropdown();
     });
   });
-  document.getElementById('theme-checkbox').addEventListener('click', function () {
-    toggleTheme();
-  });
-  document.querySelectorAll('.link-projects-perso').forEach(item => {
-    item.addEventListener('click', function () {
-      scrollToSection('projects-perso');
+  document
+    .getElementById("theme-checkbox")
+    .addEventListener("click", function () {
+      toggleTheme();
+    });
+  document.querySelectorAll(".link-projects-perso").forEach((item) => {
+    item.addEventListener("click", function () {
+      scrollToSection("projects-perso");
     });
   });
-  document.querySelectorAll('.link-projects-edu').forEach(item => {
-    item.addEventListener('click', function () {
-      scrollToSection('projects-edu');
+  document.querySelectorAll(".link-projects-edu").forEach((item) => {
+    item.addEventListener("click", function () {
+      scrollToSection("projects-edu");
     });
   });
+  document.querySelectorAll(".link-about").forEach((item) => {
+    item.addEventListener("click", function () {
+      scrollToSection("about");
+    });
+  });
+  document.querySelectorAll(".link-experience").forEach((item) => {
+    item.addEventListener("click", function () {
+      scrollToSection("experience");
+    });
+  });
+
+  const numberOfSections = document.querySelectorAll("section").length - 1;
 });
 
 // handle the resize event for the header
 function resizeHeader() {
-  let header = document.getElementById("header")
-  let toggle = document.getElementById("toggle-switch")
+  let header = document.getElementById("header");
+  let toggle = document.getElementById("toggle-switch");
   let scrollTreshold = 50;
   let scrollValue = window.scrollY || document.documentElement.scrollTop;
   if (scrollValue > scrollTreshold) {
-    header.classList.add('small-header');
-    toggle.classList.add('small-toggle');
+    header.classList.add("small-header");
+    toggle.classList.add("small-toggle");
   } else {
-    header.classList.remove('small-header');
-    toggle.classList.remove('small-toggle');
+    header.classList.remove("small-header");
+    toggle.classList.remove("small-toggle");
   }
 }
 
 // set the theme based on the saved theme
 function setSavedTheme() {
-  let body = document.querySelector('body');
-  let savedTheme = localStorage.getItem('theme');
+  let body = document.querySelector("body");
+  let savedTheme = localStorage.getItem("theme");
   if (savedTheme) {
     body.classList = savedTheme;
   }
-  document.getElementById('theme-checkbox').checked = body.classList.contains('light-theme');
+  document.getElementById("theme-checkbox").checked =
+    body.classList.contains("light-theme");
 }
 
 // toggle the theme
 function toggleTheme() {
-  let body = document.querySelector('body');
-  if (body.classList.contains('dark-theme')) {
-    body.classList.remove('dark-theme');
-    body.classList.add('light-theme');
+  let body = document.querySelector("body");
+  if (body.classList.contains("dark-theme")) {
+    body.classList.remove("dark-theme");
+    body.classList.add("light-theme");
   } else {
-    body.classList.remove('light-theme');
-    body.classList.add('dark-theme');
+    body.classList.remove("light-theme");
+    body.classList.add("dark-theme");
   }
-  localStorage.setItem('theme', body.classList);
+  localStorage.setItem("theme", body.classList);
 }
 
 // update the clock
@@ -114,30 +132,31 @@ function updateClock() {
 // toggle the dropdown menu
 function toggleDropdown() {
   let dropdown = document.getElementById("dropdown");
-  dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
+  dropdown.style.display =
+    dropdown.style.display === "block" ? "none" : "block";
 }
 
 // initialize the swiper
 function initSwiper(suffix) {
-  const swiper = new Swiper('.swiper-'+suffix, {
+  const swiper = new Swiper(".swiper-" + suffix, {
     loop: true,
     slidePerView: 1,
     centeredSlides: true,
     spaceBetween: 50,
 
-    keyboard : {
+    keyboard: {
       enabled: true,
       onlyInViewport: false,
     },
 
     pagination: {
-      el: '.swiper-pagination-'+suffix,
+      el: ".swiper-pagination-" + suffix,
       clickable: true,
     },
 
     navigation: {
-      nextEl: '.swiper-button-next-'+suffix,
-      prevEl: '.swiper-button-prev-'+suffix,
+      nextEl: ".swiper-button-next-" + suffix,
+      prevEl: ".swiper-button-prev-" + suffix,
     },
   });
 }
@@ -145,30 +164,48 @@ function initSwiper(suffix) {
 // scroll to a section
 function scrollToSection(sectionId) {
   const section = document.getElementById(sectionId);
-  window.scrollTo(0,0,behavior='instant');
-  const yValue = section.getBoundingClientRect().top + window.scrollY - (6 * window.innerHeight / 100);
+  window.scrollTo(0, 0, (behavior = "instant"));
+  const yValue =
+    section.getBoundingClientRect().top +
+    window.scrollY -
+    (6 * window.innerHeight) / 100;
   window.scrollTo({
     top: yValue,
-    behavior: 'instant'
+    behavior: "instant",
   });
 }
 
 // refresh the progress bar
 function refreshProgressBar() {
   let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  let height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
   let scrolled = (winScroll / height) * 100;
-  document.getElementById("progress-bar").style.width = scrolled + "%";
+  let progressInterval = Math.round(scrolled / 25) * 25;
+  document.getElementById("progress-bar").style.width = progressInterval + "%";
 }
 
 // update projects links
 function updateProjectsLinks() {
-  const links = document.querySelectorAll('.project-link .link');
-  var mediaQuery = window.matchMedia('(max-width: 767px)');
+  const links = document.querySelectorAll(".project-link .link");
+  var mediaQuery = window.matchMedia("(max-width: 767px)");
 
   if (mediaQuery.matches) {
-    links.forEach(link => {
-      link.innerHTML = 'Lien Github';
+    links.forEach((link) => {
+      link.innerHTML = "Lien Github";
     });
   }
+}
+
+// update the .dynamic class texts
+function updateDynamicTexts() {
+  // update the age
+  const ageText = document.getElementById("age");
+  const diff = new Date(Date.now() - new Date("2004-03-22"));
+  ageText.innerHTML = Math.abs(diff.getUTCFullYear() - 1970);
+
+  // update the copyright footer
+  const yearText = document.getElementById("copyright");
+  yearText.innerHTML = new Date().getFullYear();
 }
